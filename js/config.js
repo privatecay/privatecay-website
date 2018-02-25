d536b835f77c6c89c57523e3188c4f4cd64fce22
Bill Butler <bill@butler.net>
2018-02-25 12:31:05 -0600

    base website

---
+++ js/config.js
@@ -0,0 +1,29 @@
+/******************************************************************************************************************/
+/* ADRIANO | Creative Multipurpose Template                                                                       */
+/* (c) 2017 Rene Puchinger                                                                                        */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["Come On In.", "Stay A While."];
+
+/* Enable the Typed effect */
+var config_enableTyped = true;
+
+/* Disable Parallax effect on mobile/touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Front page effect - can be 'none', 'particles', 'video'  */
+var config_frontPageEffect = 'none';
+
+/* If config_frontPageEffect is set to video, here set the video URL, type and opacity */
+var config_videoUrl = 'video/background.mp4';
+var config_videoType = 'video/mp4';
+var config_videoOpacity = 1;
+
+/* Google map parameters */
+var config_mapLat = 25.613157; // latitude
+var config_mapLng =  -77.731295; // longitude
+var config_mapZoom = 15; // map zoom
+var config_mapTitle = 'PrivateCay' // marker title
